8665a12d668874f7581bf170a2055b696fe4376d
Lars Lunde <psyll6@nottingham.ac.uk>
2020-08-04 14:46:38 +0100

    fixed phone validation

--- src/App.js
+++ src/App.js
@@ -102,10 +102,6 @@ const MyPhoneNumber = ({ label, ...props }) => {
     );
   };
 
-
-
-const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
-
 const websiteRegExp = /^((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/;
 
 const numberRegExp = /^[0-9]{1,30}?$/;
@@ -170,7 +166,6 @@ const SignupForm = () => {
             .email("Invalid email addresss")
             .required("Required"),
           phone_number: Yup.string()
-            .matches(phoneRegExp, "Please enter a valid phone number")
             .required("Required"),
           incorp_country: Yup.string()
             .oneOf(
